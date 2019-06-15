668353b3daae9bf73fa151282f203e3c9a48e059
robotyka <48207768+robotyka@users.noreply.github.com>
2019-06-15 19:26:50 +0200

    Update and rename Lekcja1.ino to Redmerski_Mateusz_Matkowska_Zuzanna_Martyna_Milosz_2019.ino

--- Redmerski_Mateusz_Matkowska_Zuzanna_Martyna_Milosz_2019.ino
+++ Redmerski_Mateusz_Matkowska_Zuzanna_Martyna_Milosz_2019.ino
@@ -29,7 +29,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
 
-  //silniki
+  //inicjalizacja silnikow, ustawienie kierunku
   pinMode(X_STEP_PIN,OUTPUT);
   pinMode(X_DIR_PIN,OUTPUT);
   pinMode(X_ENABLE_PIN,OUTPUT);
@@ -41,7 +41,7 @@ void setup() {
   pinMode(Z_STEP_PIN,OUTPUT);
   pinMode(Z_DIR_PIN,OUTPUT);
   pinMode(Z_ENABLE_PIN,OUTPUT);
-
+  
   digitalWrite(X_ENABLE_PIN,LOW);
   digitalWrite(X_DIR_PIN,LOW);
 
@@ -51,7 +51,7 @@ void setup() {
   digitalWrite(Z_ENABLE_PIN,LOW);
   digitalWrite(Z_DIR_PIN,LOW);
 
-  //krancowki
+  //ustawienie pinow, odpowiadajacych za krancowki jako wejscia z pullupem
   pinMode(X_MAX_PIN,INPUT_PULLUP);
   pinMode(Y_MAX_PIN,INPUT_PULLUP);
   pinMode(Z_MAX_PIN,INPUT_PULLUP);
@@ -61,17 +61,21 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(i==500)
   {
+    //część kodu odpowiadająca za zmianę kierunku
     i=0;
     kierunek=!kierunek;
     digitalWrite(X_DIR_PIN,kierunek);
     digitalWrite(Y_DIR_PIN,kierunek);
     digitalWrite(Z_DIR_PIN,kierunek);
   }
-
+  
+  //dzialanie silnikow
   digitalWrite(X_STEP_PIN,LOW);
   digitalWrite(Y_STEP_PIN,LOW);
   digitalWrite(Z_STEP_PIN,LOW);
   delay(1);
+  
+  //sprawdzenie czy zaden z silnikow nie dotyka krancowki
   if((digitalRead(X_MAX_PIN)==0 && kierunek==0) || kierunek==1)
     digitalWrite(X_STEP_PIN,HIGH);
   if((digitalRead(Y_MAX_PIN)==0 && kierunek==0) || kierunek==1)
@@ -80,7 +84,7 @@ void loop() {
     digitalWrite(Z_STEP_PIN,HIGH);
   delay(1);
   i++;
-
+  
   Serial.print(digitalRead(X_MAX_PIN));
   Serial.print(digitalRead(Y_MAX_PIN));
   Serial.println(digitalRead(Z_MAX_PIN));
